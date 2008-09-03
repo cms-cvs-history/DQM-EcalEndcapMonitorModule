0250bef6a72275800f2dc82d2bc9dfa3f4197238
Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch>
2008-09-03 16:20:43 +0000

    - remove obsolete parameter

--- test/python/monitor-p5_gt_cfg.py
+++ test/python/monitor-p5_gt_cfg.py
@@ -49,7 +49,6 @@ process.dqmQTestEE = cms.EDFilter("QualityTester",
 )
 
 process.dqmSaverEE = cms.EDFilter("DQMFileSaver",
-    fileName = cms.untracked.string('EcalEndcap'),
     dirName = cms.untracked.string('.'),
     convention = cms.untracked.string('Online')
 )
